c69a4bce0d8d10d7fb6bdd4ee385ea4f7edae9fd
JRob <roberts.johns@gmail.com>
2020-02-04 13:16:06 -0800

    complete external API tasks

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,30 @@
+/*
+$(document).ready(function() {
+  getWeather();
+});
+*/
+
+function getWeather(searchQuery) {
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchQuery + '&units=imperial&APPID=' + apiKey;
+  $('.city').text("");
+  $('.temp').text("");
+  $('.error-message').text("");
+  $.ajax(url, {
+    success: function (data) {
+      console.log(data);
+      $('.city').text(data.name);
+      $('.temp').text(data.main.temp);
+    }, error: function (error) {
+      $('.error-message').text('An error occured.');
+    }
+  })
+}
+
+function searchWeather() {
+  let searchQuery = $('.search').val();
+  getWeather(searchQuery);
+};
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
