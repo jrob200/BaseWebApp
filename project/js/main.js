5e6299aab2cef84f44694f1ce828293590a34ba8
JRob <roberts.johns@gmail.com>
2020-02-04 15:27:12 -0800

    complete database tasks

--- project/js/main.js
+++ project/js/main.js
@@ -4,6 +4,48 @@ $(document).ready(function() {
 });
 */
 
+$(document).ready(function() {
+  getPosts();
+});
+
+function handleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  console.log(user.email);
+};
+
+function addMessage(postTitle, postBody) {
+  let postData = {
+    title: postTitle,
+    body: postBody
+  }
+  let database = firebase.database().ref('posts');
+
+  let newPostRef = database.push();
+  newPostRef.set(postData, function(error) {
+    if (error) {
+      console.log('write failed');
+    } else {
+      window.location.reload();
+    }
+  });
+};
+
 function getWeather(searchQuery) {
   let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchQuery + '&units=imperial&APPID=' + apiKey;
   $('.city').text("");
@@ -18,11 +60,30 @@ function getWeather(searchQuery) {
       $('.error-message').text('An error occured.');
     }
   })
-}
+};
 
 function searchWeather() {
   let searchQuery = $('.search').val();
   getWeather(searchQuery);
+};
+
+function handleMessageFormSubmit() {
+  let postTitle = $("#post-title").val();
+  let postBody = $("#post-body").val();
+  console.log(postBody);
+  addMessage(postTitle, postBody);
+};
+
+function getPosts() {
+  return firebase.database().ref('posts').once('value').then(function(snapshot) {
+    let posts = snapshot.val();
+    console.log(posts);
+
+    for (let postKey in posts) {
+      let post = posts[postKey];
+      $("#post-listing").append("<div class='post'>"+post.title+" - "+post.body+"</div>");
+    }
+  })
 };
 
 function showPicture(){
@@ -33,4 +94,4 @@ function showPicture(){
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
-}
+};
